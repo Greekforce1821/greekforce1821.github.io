06d941cfede977cc73c81998574cb2addad4be85
Greekforce1821 <spiroskoeu@gmail.com>
2023-02-24 00:49:00 +0200

    fixed some bugs #2 ...

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230223.235611';
+const cacheName = 'chirpy-20230224.004816';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
